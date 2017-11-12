8b1955fb65ceee9d294eb2bdcdf5ecbf20bd6671
usausa <machi.pon@gmail.com>
2017-11-12 18:55:42 +0900

    Add project

---
+++ app/src/main/java/usausa/github/io/example/MainActivity.kt
@@ -0,0 +1,12 @@
+package usausa.github.io.example
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
